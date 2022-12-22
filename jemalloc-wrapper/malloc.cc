a2f6a359d564f87cbb9470f1871fb222ae13f096
Fangming Lu <lfm1012@sjtu.edu.cn>
2022-12-22 14:50:51 +0800

    optimize

--- jemalloc-wrapper/malloc.cc
+++ jemalloc-wrapper/malloc.cc
@@ -4,48 +4,33 @@
 
 #include "include/allocator.hh"
 
-//using Alloc = AllocatorMaster<73>;
-
 static void *(*real_malloc)(size_t) = NULL;
 
 static void (*real_free)(void *) = NULL;
 
 
-static void __attribute__((constructor)) init(void) {
-//    real_malloc = (void *(*)(size_t)) dlsym(RTLD_NEXT, "malloc");
-//    real_free = (void (*)(void *)) dlsym(RTLD_NEXT, "free");
-
-    // Init for allocator
+static void init(void) {
+#if 1
     AllocHelper::_init(0x4ffff5a00000, 1024 * 1024 * 1024 * 1);
-//    AllocHelper::_malloc(32);
+    real_malloc = (void *(*)(size_t)) dlsym(RTLD_NEXT, "malloc");
+    real_free = (void (*)(void *)) dlsym(RTLD_NEXT, "free");
+#else
+    // Init for allocator
     real_malloc = AllocHelper::_malloc;
     real_free = AllocHelper::_free;
+#endif
 }
 
-
-extern "C"
-void *malloc(size_t len) {
-    static __thread int no_hook = 0;
-    if (!real_malloc) return 0;
-
-    if (no_hook) {
-        return (*real_malloc)(len);
-    }
-
-    void *caller = (void *) (long) __builtin_return_address(0);
-    no_hook = 1;
-    printf("malloc with len [%zu] from caller[%p]\n", len, caller); //printf call malloc internally
-    no_hook = 0;
-
-    void *ret = (*real_malloc)(len);
-
-    return ret;
+void *malloc(size_t size) {
+    if (!real_malloc) init();
+    void *ptr = real_malloc(size);
+    fprintf(stderr, "malloc(%zd) = %p\n", size, ptr);
+    return ptr;
 }
 
-extern "C"
 void free(void *ptr) {
-    if (!real_free) return;
-    void *caller = (void *) (long) __builtin_return_address(0);
-    printf("free ptr %p from caller[%p]\n", ptr, caller);
-    (*real_free)(ptr);
+    if (!real_free) init();
+    if (!ptr) return;
+    real_free(ptr);
+    fprintf(stderr, "free(%p)\n", ptr);
 }
